c979cb49bdae3d3b795d994c9a11161faae4b997
Your Name <you@example.com>
2026-02-11 17:41:16 +0300

    debug: Disable i18n_patterns to isolate crash

--- config/urls.py
+++ config/urls.py
@@ -2,22 +2,19 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns # Disabled for debugging
 from django.conf.urls.static import static
 
 # URLs that should not be translated
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    # path('i18n/', include('django.conf.urls.i18n')), # Disabled
     path('admin/', admin.site.urls),
-    path('health/', include('health_check.urls')),
-    path('api/memory/', include('project_memory.urls')), # Added Memory API
-]
-
-# URLs that should be translated
-urlpatterns += i18n_patterns(
+    # path('health/', include('health_check.urls')), # Disabled until apps are re-enabled
+    # path('api/memory/', include('project_memory.urls')), # Disabled until apps are re-enabled
+    
+    # Adding coredata urls directly without i18n
     path('', include('coredata.urls')),
-    # Add other app URLs here if they need to be translated
-)
+]
 
 # Serve media files in development
 if settings.DEBUG:
