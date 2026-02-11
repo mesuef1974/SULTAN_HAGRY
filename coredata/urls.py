6acbf74a778e20b5d95ee8583d6d0620428b5fca
Your Name <you@example.com>
2026-02-12 01:44:30 +0300

    Fix NoReverseMatch: Re-add logout URL to fix navigation rendering

--- coredata/urls.py
+++ coredata/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from coredata.views import dashboard_views, plan_views, auth_views # Import from coredata
+from coredata.views import dashboard_views, plan_views, auth_views
+from django.contrib.auth import views as auth_builtin_views
 
 urlpatterns = [
     path('', dashboard_views.dashboard_view, name='dashboard'),
@@ -7,4 +8,5 @@ urlpatterns = [
         path('', plan_views.plan_list, name='plan_list'),
     ])),
     path('auth/login/', auth_views.CustomLoginView.as_view(), name='login'),
+    path('auth/logout/', auth_builtin_views.LogoutView.as_view(), name='logout'),
 ]
